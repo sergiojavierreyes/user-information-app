0ec76a0261096c865470f69108f764fa123dceca
Sergio Reyes <sergio.javier.reyes@me.com>
2016-10-18 15:56:46 +0200

    Part 0 finished

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ const fs= require('fs')
 const app = express()
 
 app.set('view engine', 'pug')
+app.set('views', __dirname + "/views")
+
+app.get('/users', (req,res)=> {
+	fs.readFile(__dirname + '/users.json', (err, data)=> {
+		if (err) throw (err)
+			let parsedData = JSON.parse(data)
+		res.render('users', {data:parsedData})
+	})
+})
 
 app.listen(8000,() => {
-	console.log('Up and Running!')
+	console.log('Up and Running at port 8000!')
 })
