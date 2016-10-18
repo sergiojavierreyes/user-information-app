14e9337784fc85c6832a94d77ce3720840f1de68
Sergio Reyes <sergio.javier.reyes@me.com>
2016-10-18 14:43:05 +0200

    added index.pug

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 //Include the necessary modules
+const express = require('express')
+const fs= require('fs')
+const app = express()
+
+app.set('view engine', 'pug')
+
+app.listen(8000,() => {
+	console.log('Up and Running!')
+})
